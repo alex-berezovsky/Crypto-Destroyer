ae9dbd51289186cfddca7245a60536742d1c3fbb
noahsb1 <71232430+noahsb1@users.noreply.github.com>
2022-02-10 09:57:17 -0500

    Added start screen

--- buyHighSellLow/app/src/main/java/com/example/buyhighselllow/MainActivity.java
+++ buyHighSellLow/app/src/main/java/com/example/buyhighselllow/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.buyhighselllow;
 
+import android.content.Intent;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.startButton);
+        Button endButton = findViewById(R.id.endButton);
+
+        endButton.setOnClickListener(v -> System.exit(0));
+        startButton.setOnClickListener(v -> startingTheGame());
+    }
+
+    private void startingTheGame() {
+        Intent intent = new Intent(MainActivity.this, startMenu.class);
+        startActivity(intent);
     }
 }
---
+++ buyHighSellLow/app/src/main/java/com/example/buyhighselllow/startMenu.java
@@ -0,0 +1,13 @@
+package com.example.buyhighselllow;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class startMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_menu);
+    }
+}
