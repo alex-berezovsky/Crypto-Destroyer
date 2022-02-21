54f1a06f8af1ecdc9d738552825d186cc05da0ff
Devansh Singh <dsingh90@gatech.edu>
2022-02-20 22:09:42 -0500

    Adding map and monument placeholder
    adding several images and TextViews to the GameScreen to showcase where map and monument would go

--- app/src/main/java/com/example/buyhighselllow/GameScreen.java
+++ app/src/main/java/com/example/buyhighselllow/GameScreen.java
@@ -18,16 +18,25 @@ public class GameScreen extends AppCompatActivity {
     playerNameText.setText(playerName);
     playerNameText.setTextSize(30);
 
+    TextView mapPlaceholderText = findViewById(R.id.mapPlaceholder);
+    mapPlaceholderText.setText("Map/Path goes here");
+
+    TextView monumentHealthText = findViewById(R.id.monumentHealth);
     TextView playerCashText = findViewById(R.id.playerCash);
     int cash = 0;
+    int monumentHealth = 0;
     switch (difficulty){
       case 0: cash = 1000;
+              monumentHealth = 100;
               break;
       case 1: cash = 800;
+              monumentHealth = 80;
               break;
       case 2: cash = 600;
+              monumentHealth = 60;
               break;
     }
+    monumentHealthText.setText("Monument HP: " + monumentHealth);
     playerCashText.setText("Player Cash: " + cash);
     playerCashText.setTextSize(15);
   }
