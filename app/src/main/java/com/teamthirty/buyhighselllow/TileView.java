d561827c89c2f7bf5d830b7802632f3ed1526824
Berezovsky, Alex <aberezovsky3@gatech.edu>
2022-03-05 19:47:24 -0500

    Added Tile and TileView Class

---
+++ app/src/main/java/com/teamthirty/buyhighselllow/TileView.java
@@ -0,0 +1,61 @@
+package com.teamthirty.buyhighselllow;
+
+import android.graphics.Rect;
+
+public class TileView {
+
+    private int tileWidth;
+    private int tileHeight;
+    private int rows;
+    private int columns;
+    private int tileCount;
+    private Tile[][] tiles;
+
+    /**
+     * Tileview constructor which makes all the tiles for unit placement on the game screen.
+     * rows = amount of rows in grid layout
+     * columns = amount of columns in grid layout
+     * tileWidth = width of gridlayout(in dp) / columns
+     * tileHeight = height of gridlayout(in dp) / rows
+     * titleCount = number of rows * number of columns  - might not need this variable
+     *
+     * tiles[][] = 2D array that stores tiles which have 2 attributes, occupied(boolean) and
+     * Rectangle(Rect)
+     * Loops through all rows and columns and creates a tile with a rectangle of appropriate coords
+     *
+     * We can check if a tile is occupied by looping through all towers and comparing their state
+     * coords with Tile's coords(there are methods for it for Rects)
+     */
+
+    public TileView() {
+
+        rows = 12;
+        columns = 12;
+        tileWidth = 720 / columns;
+        tileHeight = 408 / rows;
+        tileCount = rows * columns;
+
+        for (int i = 0; i < rows; i++) {
+
+            int top = rows * tileHeight;
+            int bottom = (rows + 1) * (tileHeight);
+
+            for (int j = 0; j < columns; j++) {
+
+                int left = columns * tileWidth;
+                int right = (columns + 1) * tileWidth;
+                Rect rectangle = new Rect(left, top, right, bottom);
+                tiles[i][j] = new Tile(false, rectangle);
+
+            }
+
+        }
+
+
+    }
+
+    public int getTileCount() {
+        return tileCount;
+    }
+
+}
