cc1d950751e28e8a797d3f84b1bb98622c5071ad
noahsb1 <71232430+noahsb1@users.noreply.github.com>
2022-03-10 13:54:12 -0500

    when you click a tower and then click a valid tile, it sets the color of the tile to the color of the tower and decrements money

--- app/src/main/java/com/teamthirty/buyhighselllow/Screens/GameScreen.java
+++ app/src/main/java/com/teamthirty/buyhighselllow/Screens/GameScreen.java
@@ -2,20 +2,28 @@ package com.teamthirty.buyhighselllow.Screens;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import androidx.gridlayout.widget.GridLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.teamthirty.buyhighselllow.Entities.Towers.*;
+import com.teamthirty.buyhighselllow.Systems.PlayerSystem;
 import com.teamthirty.buyhighselllow.Utilities.Difficulty;
 import com.teamthirty.buyhighselllow.R;
 import com.teamthirty.buyhighselllow.Utilities.TowerType;
 import androidx.core.util.Pair;
 import java.util.ArrayList;
 
-public class GameScreen extends AppCompatActivity {
+public class GameScreen extends AppCompatActivity implements View.OnClickListener {
     private GridLayout mapLayout;
     private ArrayList<Pair<Integer, Integer>> path;
+    private Button[][] mapArray;
+    private TowerType towerType;
+    private PlayerSystem playerSystem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,7 @@ public class GameScreen extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         String playerName = extras.getString("name");
         Difficulty difficulty = (Difficulty) extras.get("difficulty");
-        TowerType towerType = null;
+        playerSystem = new PlayerSystem(difficulty);
 
         // User interface buttons
         Button redditDude = findViewById(R.id.RedditDude);
@@ -43,9 +51,9 @@ public class GameScreen extends AppCompatActivity {
         path.add(new Pair<>(3, 0));
         makeMap();
 
-        redditDude.setOnClickListener(view -> setTowerType(towerType, TowerType.RedditDude));
-        tradingChad.setOnClickListener(view -> setTowerType(towerType, TowerType.TradingChad));
-        cryptoWhale.setOnClickListener(view -> setTowerType(towerType, TowerType.CryptoWhale));
+        redditDude.setOnClickListener(view -> setTowerType(TowerType.RedditDude));
+        tradingChad.setOnClickListener(view -> setTowerType(TowerType.TradingChad));
+        cryptoWhale.setOnClickListener(view -> setTowerType(TowerType.CryptoWhale));
 
         TextView playerNameText = findViewById(R.id.playerName);
         playerNameText.setText(playerName);
@@ -70,19 +78,27 @@ public class GameScreen extends AppCompatActivity {
             monumentHealth = 100;
             break;
         }
+        playerSystem.setMoney(cash);
+
         monumentHealthText.setText("Monument HP: " + monumentHealth);
         playerCashText.setText("Player Cash: " + cash);
         playerCashText.setTextSize(15);
+
+        for (Button[] buttonArray : mapArray) {
+            for (Button button : buttonArray) {
+                button.setOnClickListener(this);
+            }
+        }
     }
 
-    private void setTowerType(TowerType towerType, TowerType newType) {
+    private void setTowerType(TowerType newType) {
         towerType = newType;
     }
 
     private void makeMap() {
         // Layout object representing the map
         GridLayout mapLayout = findViewById(R.id.map);
-        Button[][] mapArray = new Button[mapLayout.getRowCount()][mapLayout.getColumnCount()];
+        mapArray = new Button[mapLayout.getRowCount()][mapLayout.getColumnCount()];
         for (int i = 0; i < mapLayout.getRowCount(); i++) {
             for (int j = 0; j < mapLayout.getColumnCount(); j++) {
                 Pair<Integer, Integer> temp = new Pair<>(i, j);
@@ -95,7 +111,10 @@ public class GameScreen extends AppCompatActivity {
                 }
             }
         }
-
+        Pair<Integer, Integer> monumentLocation = path.get(0);
+        int monumentRow = monumentLocation.first;
+        int monumentColumn = monumentLocation.second;
+        mapArray[monumentRow][monumentColumn].setBackgroundColor(Color.MAGENTA);
     }
 
     private Button makeMapButton(GridLayout mapLayout, Context context, int color, int row,
@@ -108,9 +127,78 @@ public class GameScreen extends AppCompatActivity {
 
         //Buttons on the grid that handle tower press placement
         Button button = new Button(context);
+        String id;
+        if (row == 0) {
+            id = "999" + column;
+        } else {
+            id = row + "000" + column;
+        }
+        button.setId(Integer.parseInt(id));
         button.setBackgroundColor(color);
         button.setLayoutParams(buttonParams);
 
         return button;
+    }
+
+    @Override
+    public void onClick(View v) {
+        String id = v.getId() + "";
+        int row;
+        int column;
+
+        if (id.contains("999")) {
+            row = 0;
+            column = Integer.parseInt(id.substring(3));
+        } else {
+            int endOfRowIndex = id.indexOf('0');
+            row = Integer.parseInt(id.substring(0, endOfRowIndex));
+            column = Integer.parseInt(id.substring(endOfRowIndex + 3));
+        }
+        Tower tower = null;
+        Pair<Integer, Integer> towerLocation = new Pair<>(row, column);
+
+        if (path.contains(towerLocation)) {
+            String errorMessage = "Cannot place tower on path!";
+            int popUpDuration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(this, errorMessage, popUpDuration);
+            toast.show();
+        } else {
+            if (towerType == null) {
+                String errorMessage = "Select a tower type to place!";
+                int popUpDuration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(this, errorMessage, popUpDuration);
+                toast.show();
+            } else {
+                if (((ColorDrawable) mapArray[row][column].getBackground()).getColor()
+                    != (Color.GREEN)) {
+                    String errorMessage = "Cannot place tower on top of another tower!";
+                    int popUpDuration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(this, errorMessage, popUpDuration);
+                    toast.show();
+                } else {
+                    if (towerType.equals(TowerType.RedditDude)) {
+                        tower = new RedditDude(towerLocation);
+                    } else if (towerType.equals(TowerType.TradingChad)) {
+                        tower = new TradingChad(towerLocation);
+                    } else if (towerType.equals(TowerType.CryptoWhale)) {
+                        tower = new CryptoWhale(towerLocation);
+                    }
+
+                    //playerSystem.addEntity(tower);
+                    boolean towerBought = playerSystem.buyTower(tower, towerType, this);
+                    if (towerBought) {
+                        if (towerType.equals(TowerType.RedditDude)) {
+                            mapArray[row][column].setBackgroundColor(Color.YELLOW);
+                        } else if (towerType.equals(TowerType.TradingChad)) {
+                            mapArray[row][column].setBackgroundColor(Color.BLUE);
+                        } else if (towerType.equals(TowerType.CryptoWhale)) {
+                            mapArray[row][column].setBackgroundColor(Color.BLACK);
+                        }
+                        TextView playerCashText = findViewById(R.id.playerCash);
+                        playerCashText.setText("Player Cash: " + playerSystem.getMoney());
+                    }
+                }
+            }
+        }
     }
 }
