7a7922a528a973770dd856cbe9ebb993ed17348e
Devansh Singh <dsingh90@gatech.edu>
2022-03-09 21:50:09 -0500

    Updated button/textview locations
    Updated button/textview locations to get rid of overlap

--- app/src/main/java/com/teamthirty/buyhighselllow/Screens/GameScreen.java
+++ app/src/main/java/com/teamthirty/buyhighselllow/Screens/GameScreen.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import com.teamthirty.buyhighselllow.TileView;
 import com.teamthirty.buyhighselllow.Utilities.Difficulty;
 import com.teamthirty.buyhighselllow.R;
 import com.teamthirty.buyhighselllow.Utilities.TowerType;
