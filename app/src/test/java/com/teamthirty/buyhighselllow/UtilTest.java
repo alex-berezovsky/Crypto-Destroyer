46a208c402d96eb02ff26b0cf14fdd656c40af25
Akshin Vemana <avemana6@gatech.edu>
2022-03-11 01:01:47 -0500

    Add tests for name sanitization check

---
+++ app/src/test/java/com/teamthirty/buyhighselllow/UtilTest.java
@@ -0,0 +1,29 @@
+package com.teamthirty.buyhighselllow;
+
+import com.teamthirty.buyhighselllow.Utilities.Util;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UtilTest {
+
+    @Test
+    public void nullNameInput() {
+
+        String name = null;
+        Assert.assertFalse(Util.sanitizeNameInput(null, name));
+
+    }
+
+    @Test
+    public void emptyNameInput() {
+
+        String name = "";
+        Assert.assertFalse(Util.sanitizeNameInput(null, name));
+
+        name = "     ";
+        Assert.assertFalse(Util.sanitizeNameInput(null, name));
+
+    }
+
+
+}
