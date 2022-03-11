d0723b09ee717de9e7091d473be8fdc353df0d7d
Devansh Singh <dsingh90@gatech.edu>
2022-03-11 00:26:27 -0500

    Update TowerTests.java
    Added tests for RedditDude instantiation, level up, and generate projectile

--- app/src/test/java/com/teamthirty/buyhighselllow/TowerTests.java
+++ app/src/test/java/com/teamthirty/buyhighselllow/TowerTests.java
@@ -47,7 +47,7 @@ public class TowerTests {
     }
 
     /**
-     * Tests is projectile behaves correctly
+     * Tests if projectile behaves correctly
      */
     @Test
     public void projectileTradingChad() {
@@ -64,4 +64,65 @@ public class TowerTests {
         assertEquals(30, proj.getPosition().first.intValue());
         assertEquals(35, proj.getPosition().second.intValue());
     }
+
+    /**
+     * Tests proper instantiation of a RedditDude tower object
+     */
+    @Test
+    public void redditDudeInstantiation() {
+        Pair<Integer, Integer> location = new Pair<>(10, 15);
+        RedditDude redditDude = new RedditDude(location);
+
+        assertEquals(1, redditDude.getLevel());
+        assertEquals(10, redditDude.getPosition().first.intValue());
+        assertEquals(15, redditDude.getPosition().second.intValue());
+        assertEquals(20, redditDude.getFireRate());
+        assertEquals(10, redditDude.getDamage());
+        assertEquals(250, redditDude.getUpgradeCost());
+    }
+
+    /**
+     * Tests proper values of a RedditDude tower when leveled up
+     */
+    @Test
+    public void levelUpRedditDude() {
+        Pair<Integer, Integer> location = new Pair<>(10, 15);
+        RedditDude redditDude = new RedditDude(location);
+
+        redditDude.levelUp();
+        assertEquals(2, redditDude.getLevel());
+        assertEquals(10, redditDude.getPosition().first.intValue());
+        assertEquals(15, redditDude.getPosition().second.intValue());
+        assertEquals(30, redditDude.getFireRate());
+        assertEquals(40, redditDude.getDamage());
+        assertEquals(2500, redditDude.getUpgradeCost());
+
+        redditDude.levelUp();
+        assertEquals(3, redditDude.getLevel());
+        assertEquals(10, redditDude.getPosition().first.intValue());
+        assertEquals(15, redditDude.getPosition().second.intValue());
+        assertEquals(40, redditDude.getFireRate());
+        assertEquals(160, redditDude.getDamage());
+        assertEquals(25000, redditDude.getUpgradeCost());
+    }
+
+    /**
+     * Tests if a RedditDude makes a correct projectile
+     */
+    @Test
+    public void projectileRedditDude() {
+        Pair<Integer, Integer> location = new Pair<>(10, 15);
+        RedditDude redditDude = new RedditDude(location);
+
+        Projectile proj = redditDude.generateProjectile();
+        assertEquals(10, proj.getPosition().first.intValue());
+        assertEquals(15, proj.getPosition().second.intValue());
+        assertEquals(20, proj.getSpeed());
+        assertEquals(10, proj.getDamage());
+
+        proj.updatePosition();
+        assertEquals(30, proj.getPosition().first.intValue());
+        assertEquals(35, proj.getPosition().second.intValue());
+    }
+
 }
