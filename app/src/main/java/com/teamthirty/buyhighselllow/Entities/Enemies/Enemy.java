5a9d165cb6a231d4241d7345d4d76cc4177e4b12
noahsb1 <71232430+noahsb1@users.noreply.github.com>
2022-03-22 00:03:32 -0400

    created enemy

---
+++ app/src/main/java/com/teamthirty/buyhighselllow/Entities/Enemies/Enemy.java
@@ -0,0 +1,10 @@
+package com.teamthirty.buyhighselllow.Entities.Enemies;
+
+import com.teamthirty.buyhighselllow.Components.TransformComponent;
+import com.teamthirty.buyhighselllow.Entities.Entity;
+
+public class Enemy extends Entity implements TransformComponent {
+    public Enemy() {
+        
+    }
+}
